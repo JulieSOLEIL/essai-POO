6d77c51380e844c7112ddaaa39a9a88f5a45ca84
Julie SUN <sun.julie0@gmail.com>
2021-12-16 16:39:21 +0100

    first commit

---
+++ Bateau.php
@@ -0,0 +1,25 @@
+<?php
+
+class Bateau extends Vehicule {
+
+    private $nbrCoque;
+
+    public function __construct(string $marque, string $energie, $coque, int $ch = 10) {
+     
+        parent::__construct($marque, $energie, $ch);
+        $this->nbrCoque = $coque;
+
+    }
+
+    public function seDeplacer() {
+        
+        echo 'Je navigue';
+    }
+
+    public function __toString() {
+        
+        $info = parent::__toString();
+        $info .= "\nj'ai ".$this->nbrCoque." coques";
+        return $info;
+    }
+}
